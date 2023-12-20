ddc5fcd8dd02dbd83dbd227a2da01e33fbe5d6c0
Duyha2000 <duytd19405c@st.uel.edu.vn>
2023-12-20 09:47:25 +0700

    arrray method

--- Array/array.js
+++ Array/array.js
@@ -94,25 +94,112 @@ Cho 1 mảng gồm n phần tử và 1 số nguyên k được nhập từ bàn 
 -> In ra -1 (vì trong mảng không có phần tử nào = 7)
 */
 
-let array = [];
-let b = parseInt(prompt("Nhập số phần tử của mảng:"));
+// let array = [];
+// let b = parseInt(prompt("Nhập số phần tử của mảng:"));
 
-for (let i = 0; i < b; i++) {
-  let input = parseInt(prompt(`Nhap 1 so nguyen thu ${i + 1}`));
-  array.push(input);
-}
-let k = parseInt(prompt("Nhập số nguyên k:"));
-let position = -1;
-for (let i = 0; i < b; i++) {
-  if (k === array[i]) {
-    position = i;
-    break;
-  }
-}
-console.log(position);
+// for (let i = 0; i < b; i++) {
+//   let input = parseInt(prompt(`Nhap 1 so nguyen thu ${i + 1}`));
+//   array.push(input);
+// }
+// let k = parseInt(prompt("Nhập số nguyên k:"));
+// let position = -1;
+// for (let i = 0; i < b; i++) {
+//   if (k === array[i]) {
+//     position = i;
+//     break;
+//   }
+// }
+// console.log(position);
 /*
 ## Nhập và một số nguyên dương n, arr. Đếm trong mảng có tất cả bao nhiêu số nguyên tố?
 
 Input: n = 5 -> [3, 4, 5, 6, 7]
 Output: 3 (3 , 5 ,7 ) là số nguyên tố
 */
+
+let cart = ["iphone", "samsung", "sony", "lenovo"];
+// Thêm 1 sản phẩm vào giỏ hàng: ví dụ: toshiba
+cart.push("toshiba");
+console.log(cart);
+// Hiển thị số lượng sản phẩm trong giỏ hàng dùng length
+
+console.log(cart.length);
+
+// Sort: sắp xếp
+const prices = [19000, 20000, 18000, 30000, 15000];
+// Sắp xếp giá tăng dần
+// prices.sort();
+
+// Sắp xếp giá giảm dần
+// prices.sort((a, b) => b - a);
+
+// console.log(prices);
+// Đảo ngược: reverse
+// prices.reverse();
+// console.log(prices);
+// Tìm kiếm 1 vị trí phần tử trong mảng: findIndex (tìm kiếm)
+console.log(prices.findIndex((element) => element === 15000)); // 4
+const indexDelete = prices.findIndex((element) => element === 15000);
+// Reduce:
+// Tính tổng các phần tử trong mảng
+// let sum = 0;
+// for (let i = 0; i < prices.length; i++) sum += price[i];
+
+const sumArray = prices.reduce(
+  (accumulator, currentValue) => accumulator + currentValue,
+  0
+);
+console.log(sumArray);
+
+// Filter để lọc các phần tử thỏa mãn 1 điều kiện (lọc giá trong trang tìm kiếm)
+// const prices = [19000, 20000, 18000, 30000, 15000];
+
+const result = prices.filter(checkPrices);
+
+function checkPrices(price) {
+  return price >= 18000;
+}
+console.log("result", result);
+
+// Xóa 1 phần tử trong mảng (splice)
+// index - vị trí
+// how many: số phần tử cần xóa
+// Splice kết hợp với findIndex để tìm kiếm 1 phần tử trong mảng sau đó xóa phần tử đấy đi
+prices.splice(indexDelete, 1);
+console.log("After delete:", prices);
+
+// Foreach - map
+
+const idea = [50000, 70000, 90000, 120000];
+console.log("Array idea", idea);
+// Tìm kiếm và trả về sản phẩm có giá lớn hơn 80000
+// filter: [90000,120000]
+// let resultFilter = idea.filter(checkPrices);
+
+// function checkPrices(price) {
+//   return price >= 80000;
+// }
+// console.log(resultFilter);
+// Tìm kiếm giá tăng dần
+
+idea.sort(function (a, b) {
+  return b - a;
+});
+console.log("After sort", idea);
+// Tính tổng các số trong mảng idea: reduce
+
+const sumWithInitial = idea.reduce(
+  (accumulator, currentValue) => accumulator + currentValue,
+  0
+);
+console.log("Sum", sumWithInitial);
+
+// Xóa 1 phần tử có giá 90000 trong mảng idea
+// Các bước để xóa 1 phần tử: tìm vị trí (findIndex) -> xóa bằng splice
+// B1: findIndex
+
+console.log(idea.findIndex((element) => element === 90000));
+const index = idea.findIndex((element) => element === 90000); // 1
+// B2: Splice để xóa phần tử (index: vị trí, deleteCount: số phần tử muốn xóa)
+idea.splice(index, 1);
+console.log(idea);
