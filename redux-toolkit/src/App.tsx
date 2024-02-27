16c7e8e2265b9078e2740c3c4eb9e4824ec44786
Duyha2000 <duytd19405c@st.uel.edu.vn>
2024-02-27 14:34:42 +0700

    commit file

--- redux-toolkit/src/App.tsx
+++ redux-toolkit/src/App.tsx
@@ -1,5 +1,12 @@
 function App() {
-  return <></>;
+  return (
+    <>
+      <form action="">
+        <input type="text" />
+        <label htmlFor="">Xin chao</label>
+      </form>
+    </>
+  );
 }
 
 export default App;
