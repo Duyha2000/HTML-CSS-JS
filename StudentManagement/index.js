cc18591366b5c1db21116d434cd100f859bc4ce5
Duyha2000 <duytd19405c@st.uel.edu.vn>
2024-04-04 14:31:37 +0700

    students

--- StudentManagement/index.js
+++ StudentManagement/index.js
@@ -15,6 +15,7 @@ let students = JSON.parse(localStorage.getItem("students")) || [];
 
 console.log(JSON.parse(localStorage.getItem("students")));
 let tbody = document.getElementById("tbody1");
+// Hiển thị thông tin sinh viên:
 function displayData() {
   let html = "";
   for (let i in students) {
@@ -25,7 +26,7 @@ function displayData() {
     <td>${students[i].gender}</td>
     <td>${students[i].classmate}</td>
     <td>${students[i].image}</td>
-    <td><button>Edit</button></td>
+    <td><button onclick="editStudent('${students[i].id}')">Edit</button></td>
     <td><button onclick="deleteStudent('${students[i].id}')">Delete</button></td>
     </tr>  
   `;
@@ -39,6 +40,42 @@ function displayData() {
   classmate.value = "";
   image.value = "";
 }
+// Edit student:
+function editStudent(studentId) {
+  console.log(studentId, "ID");
+  localStorage.setItem("studentId", studentId);
+  document.getElementById("add").style.display = "none";
+  document.getElementById("save").style.display = "block";
+  let index = students.findIndex((student) => student.id == studentId); // 1
+  console.log(students[index]);
+  fullName.value = students[index].fullName;
+  date.value = students[index].date;
+  gender.value = students[index].gender;
+  classmate.value = students[index].classmate;
+  image.value = students[index].image;
+}
+
+function saveStudent() {
+  document.getElementById("add").style.display = "block";
+  document.getElementById("save").style.display = "none";
+  let index = students.findIndex(
+    (student) => student.id == localStorage.getItem("studentId")
+  );
+  console.log(index, "index");
+  students[index] = {
+    id: students[index].id,
+    fullName: fullName.value,
+    date: date.value,
+    gender: gender.value,
+    classmate: classmate.value,
+    image: image.value,
+  };
+
+  localStorage.setItem("students", JSON.stringify(students));
+  displayData();
+  alert("Chỉnh sửa thông tin thành công!!!");
+}
+
 // Delete student:
 function deleteStudent(studentId) {
   console.log(studentId);
@@ -53,8 +90,10 @@ function deleteStudent(studentId) {
   alert("Xóa student thành công!");
   console.log(index);
 }
+
 displayData();
 
+// Thêm sinh viên
 function addStudent() {
   const newStudent = {
     id: Math.floor(Math.random() * 1000),
@@ -68,7 +107,8 @@ function addStudent() {
   //   Lưu dữ liệu vào localStorage
   localStorage.setItem("students", JSON.stringify(students));
   displayData();
-  console.log(students);
   alert("Thêm học sinh mới thành công!!!!");
 }
 // tbody.innerHtml = ...
+
+// Chỉnh sửa dữ liệu:
